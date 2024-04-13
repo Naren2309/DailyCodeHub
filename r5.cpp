0f4c1d6d253c4a029d921b69b880af77180228f5
Narayanan S <148234500+Naren2309@users.noreply.github.com>
2024-04-13 23:28:42 +0530

    Add files via upload

---
+++ r5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void f(int i, int n)
+{
+	if(i<1)
+	return;
+	                // backtracking 1 to n
+	cout<<i<<endl;
+	f(i-1,n);   
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	f(n,n);
+}
